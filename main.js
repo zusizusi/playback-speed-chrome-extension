61534e30a37f4233ffb35dcd2c42d67f9809f3c9
zusizusi <41606073+zusizusi@users.noreply.github.com>
2021-11-14 19:00:53 +0900

    display speed

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("読み込み");
 // window.addEventListener("load", main, false);
 window.onload = function () {
@@ -5,47 +6,82 @@ window.onload = function () {
   console.log("ロード後");
   var playbackSpeed = 1;
   var video = document.querySelector("video");
+  var clientRect = video.getBoundingClientRect() ;
   console.log(video.style.width);
+  var canv = document.createElement("canvas");
+  canv.id = "canvasSpeed";
+  // canv.width = 300;
+  // canv.height = 1500;
+  // canv.style.top = 0;
+  // canv.style.left = 0;
+  // canv.setAttribute( "width" ,  );
+  // canv.setAttribute( "height" , 500 );
 
-  var canvas = document.createElement("canvas");
-  canvas.id = "canvasSpeed";
-  canvas.width = 300;
-  canvas.height = 1500;
-  canvas.style.top = 0;
-  canvas.style.left = 0;
-  //   canvas.width = video.style.width;
-  //   canvas.height = video.style.height;
-  //   canvas.style.top = video.style.top;
-  //   canvas.style.left = video.style.left;
-  canvas.style.zIndex = 10000;
-
+  canv.width = parseInt(video.style.width);
+  canv.height = parseInt(video.style.height);
+  canv.style.top = clientRect.top + "px";
+  canv.style.left = clientRect.left + "px";
+  canv.style.zIndex = 10000;
+  canv.style.position = "absolute";
+  console.log("add canvas")
+  document.body.appendChild(canv);
+  // document.body.appendChild(canvas); // adds the canvas to the body element
+  // document.getElementById('container').appendChild(canvas)
+  // document.getElementsByClassName('html5-video-container').appendChild(canv)
   window.document.onkeydown = function (event) {
+    var timerId;
     if (event.key === "d") {
       playbackSpeed += 0.1;
-      drawSpeed();
+      console.log(playbackSpeed);
+      clearDraw();
+      clearTimeout(timerId);
+      drawSpeed()
+      timerId = setTimeout(clearDraw, 3000 );
+      // drawSpeed();
     }
     if (playbackSpeed >= 0.1) {
       if (event.key === "a") {
         playbackSpeed -= 0.1;
-        drawSpeed();
+        console.log(playbackSpeed);
+        clearDraw();
+        clearTimeout(timerId);
+        drawSpeed()
+        timerId = setTimeout(clearDraw, 3000 );
+        // drawSpeed();
       }
     }
     if (event.key === "s") {
       playbackSpeed = 1;
-      drawSpeed();
+      console.log(playbackSpeed);
+      clearDraw();
+      clearTimeout(timerId);
+      drawSpeed()
+      timerId = setTimeout(clearDraw, 3000 );
     }
     video.playbackRate = playbackSpeed;
     console.log("playbackSpeed : " + playbackSpeed);
   };
 
   function drawSpeed() {
-    console.log(canvas);
-    var ctx = canvas.getContext("2d");
+    canv = document.getElementById('canvasSpeed');
+    var ctx = canv.getContext("2d");
+    ctx.clearRect(0, 0, canv.width, canv.height);
+    // ctx.translate(10,150);
     ctx.fillStyle = "gray";
-    // ctx.font = "48px serif";
-    // ctx.fillText(String(Math.round(playbackSpeed * 10) / 10), 10, 50);
+    ctx.font = "48px serif";
+    ctx.fillText(String(Math.round(playbackSpeed * 10) / 10), 10, 50);
+    // ctx.fillText("aaaaaaaaaaaaaaaa", 100, 100);
+    // var ctx = document.getElementById('canvasSpeed').getContext("2d");
+  }
+
+  function clearDraw(){
+    canv = document.getElementById('canvasSpeed');
+    var ctx = canv.getContext("2d");
+    ctx.clearRect(0, 0, canv.width, canv.height);
   }
 };
+
+
 // document.addEventListener("keydown", function (e) {
 //   console.log(e.code);
 // });
