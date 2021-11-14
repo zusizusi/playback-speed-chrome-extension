a96d2d2fa4738c41b260133485f587a2b342153e
zusizusi <41606073+zusizusi@users.noreply.github.com>
2021-11-14 20:20:35 +0900

    fix bottom

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ window.onload = function () {
   console.log("ロード後");
   var playbackSpeed = 1;
   var video = document.querySelector("video");
-  var clientRect = video.getBoundingClientRect() ;
   console.log(video.style.width);
   var canv = document.createElement("canvas");
   canv.id = "canvasSpeed";
@@ -16,25 +15,25 @@ window.onload = function () {
   // canv.style.left = 0;
   // canv.setAttribute( "width" ,  );
   // canv.setAttribute( "height" , 500 );
-
-  canv.width = parseInt(video.style.width);
-  canv.height = parseInt(video.style.height);
-  canv.style.top = clientRect.top + "px";
-  canv.style.left = clientRect.left + "px";
   canv.style.zIndex = 10000;
   canv.style.position = "absolute";
-  console.log("add canvas")
   document.body.appendChild(canv);
+  var timerId;
   // document.body.appendChild(canvas); // adds the canvas to the body element
   // document.getElementById('container').appendChild(canvas)
   // document.getElementsByClassName('html5-video-container').appendChild(canv)
   window.document.onkeydown = function (event) {
-    var timerId;
+    canv.width = parseInt(video.style.width);
+    canv.height = parseInt(video.style.height);
+    var clientRect = video.getBoundingClientRect() ;
+    canv.style.top = clientRect.top + "px";
+    canv.style.left = clientRect.left + "px";
+
     if (event.key === "d") {
       playbackSpeed += 0.1;
       console.log(playbackSpeed);
-      clearDraw();
       clearTimeout(timerId);
+      clearDraw();
       drawSpeed()
       timerId = setTimeout(clearDraw, 3000 );
       // drawSpeed();
@@ -43,8 +42,8 @@ window.onload = function () {
       if (event.key === "a") {
         playbackSpeed -= 0.1;
         console.log(playbackSpeed);
-        clearDraw();
         clearTimeout(timerId);
+        clearDraw();
         drawSpeed()
         timerId = setTimeout(clearDraw, 3000 );
         // drawSpeed();
@@ -53,8 +52,8 @@ window.onload = function () {
     if (event.key === "s") {
       playbackSpeed = 1;
       console.log(playbackSpeed);
-      clearDraw();
       clearTimeout(timerId);
+      clearDraw();
       drawSpeed()
       timerId = setTimeout(clearDraw, 3000 );
     }
@@ -68,8 +67,10 @@ window.onload = function () {
     ctx.clearRect(0, 0, canv.width, canv.height);
     // ctx.translate(10,150);
     ctx.fillStyle = "gray";
-    ctx.font = "48px serif";
-    ctx.fillText(String(Math.round(playbackSpeed * 10) / 10), 10, 50);
+    var bottom_padding = canv.height/20;
+    ctx.font = canv.height/2 - bottom_padding +"px serif";
+    console.log(playbackSpeed.toFixed(1));
+    ctx.fillText(String(playbackSpeed.toFixed(1)) + "x", canv.width/2, canv.height- bottom_padding);
     // ctx.fillText("aaaaaaaaaaaaaaaa", 100, 100);
     // var ctx = document.getElementById('canvasSpeed').getContext("2d");
   }
@@ -82,6 +83,4 @@ window.onload = function () {
 };
 
 
-// document.addEventListener("keydown", function (e) {
-//   console.log(e.code);
-// });
+
